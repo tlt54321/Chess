04ba942b35a3846f112c7e73fe53666f45a3c7cd
Stephen Cerruti <league@WTS11.attlocal.net>
2016-12-11 18:56:10 -0800

    Added some variables

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,19 @@
+import javax.swing.JButton;
 
-public class Tile {
-
+public class Tile extends JButton{
+	public static final int EMPTY = 0;
+	public static final int PAWN = 1;
+	public static final int BISHOP = 2;
+	public static final int KNIGHT = 3;
+	public static final int ROOK = 4;
+	public static final int QUEEN = 5;
+	public static final int KING = 6;
+	private int tileStatus;//number 0-6 defines what piece is on this tile
+	private int X;//x co-ordinate 0-7
+	private int Y;//y co-ordinate 0-7
+	public Tile(int tileNum, int x, int y) {
+		this.tileStatus = tileNum;
+		this.X = x;
+		this.Y = y;
+	}
 }
